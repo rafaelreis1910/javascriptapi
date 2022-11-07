a99f5eac5f74d09375d6baabb7955fdd890e373f
rafaelreis1910 <rafael.reis@aluno.ifsp.edu.br>
2022-11-06 21:45:13 -0300

    atualização banco para deploy

--- config/database.js
+++ config/database.js
@@ -4,13 +4,7 @@ mongoose.Promise = global.Promise;
 require("dotenv").config();
 const MONGO_URL = process.env.MONGO_URL;
 mongoose
-// // .connect("mongodb://localhost/javascriptnote", {
-  //   useNewUrlParser: true,
-  //   useUnifiedTopology: true,
-  //   // useCreateIndex: true, THIS IS NO LONGER SUPPORTED - it's automatically set to true
-  // })
-  //DEPLOY - change the adress of the database to the actual adress on the cloud   .connect(MONGO_URL, {
-  .connect("mongodb://localhost/javascriptnote", {
+  .connect(MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
