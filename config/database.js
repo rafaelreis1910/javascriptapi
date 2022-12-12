278df9e8a7d9edd6f4c99fe746b98f3574b3a476
Rafael Reis <80486202+rafaelreis1910@users.noreply.github.com>
2022-12-12 15:51:24 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 require("dotenv").config();
 const MONGO_URL = process.env.MONGO_URL;
 mongoose.connect(
-  "mongodb+srv://rafaelsccp10:<v15qwErp>@cluster0.3cq0sy1.mongodb.net/?retryWrites=true&w=majority", {
+  "mongodb+srv://rafaelsccp10:<v15qwErp@>@cluster0.xhuylxs.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
